2ae8b78b9632ae225282dfc73d90f72be8e92eda
MichalPawelek123 <80778573+MichalPawelek123@users.noreply.github.com>
2025-01-27 14:50:06 +0100

    Hangman game, first exercise: pick a word

---
+++ pick_a_word.py
@@ -0,0 +1,31 @@
+"""
+This exercise is Part 1 of 3 of the Hangman exercise series.
+
+In this exercise, the task is to write a function that picks a random word from a list of words
+from the SOWPODS dictionary. 
+Download this file(http://norvig.com/ngrams/sowpods.txt) and save it in the same directory
+as your Python code.
+
+This file is Peter Norvig’s compilation of the dictionary of words used in professional Scrabble
+tournaments. Each line in the file contains a single word.
+
+"""
+
+import random
+
+
+def pick_a_word(file_name): 
+    line_number = random.randint(1, 267751)
+    interation_number = 0
+    words = []
+
+    with open(f'{file_name}', 'r') as f: 
+        line = f.readline()
+        while interation_number < line_number: 
+            line = f.readline()
+            words.append(line)
+            interation_number+=1
+    return words[-1]
+
+
+print(pick_a_word('list_of_words.txt'))
